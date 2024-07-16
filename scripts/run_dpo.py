e55b05eef3133bb6dc79fffd727fbbcb55b67b7b
chrisliu298 <cyliu298@proton.me>
2024-07-16 23:25:21 +0800

    Manually set generate_during_eval

--- scripts/run_dpo.py
+++ scripts/run_dpo.py
@@ -201,6 +201,7 @@ def main():
         max_prompt_length=training_args.max_prompt_length,
         peft_config=get_peft_config(model_args),
         loss_type=training_args.loss_type,
+        generate_during_eval=False,
     )
 
     ###############
